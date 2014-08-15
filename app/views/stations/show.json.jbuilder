ab05a6b125ef5c433e074457e716e3a3568d85b6
Thadeu Brito <itvbrito@gmail.com>
2014-08-14 21:50:20 -0300

    Stations scaffold

---
+++ app/views/stations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @station, :id, :latitude, :longitude, :name, :zone, :total_lines, :rail, :created_at, :updated_at
---
+++ db/migrate/20140815004435_create_stations.rb
@@ -0,0 +1,14 @@
+class CreateStations < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.float :latitude
+      t.float :longitude
+      t.string :name
+      t.integer :zone
+      t.integer :total_lines
+      t.integer :rail
+
+      t.timestamps
+    end
+  end
+end
