a9a2028bdcb07e83de8ba61b6a654210b71cb572
Thadeu Brito <itvbrito@gmail.com>
2014-08-14 22:34:32 -0300

    Import stations data from stations.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+stations_csv_file = File.read('data/stations.csv')
+station_csv = CSV.parse(stations_csv_file, :headers => true)
+station_csv.each do |row|
+  @station = Station.new
+  @station.id          = row.to_hash['id']
+  @station.latitude    = row.to_hash['latitude']
+  @station.longitude   = row.to_hash['longitude']
+  @station.name        = row.to_hash['name']
+  @station.zone        = row.to_hash['zone']
+  @station.total_lines = row.to_hash['total_lines']
+  @station.rail        = row.to_hash['rail']
+  @station.save!
+end
